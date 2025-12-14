001354b3bdb0e719e2ad5299e0fc03131d13fe1e
suzu55-55 <k.suz0224@gmail.com>
2025-12-14 20:10:47 +0900

    Add files via upload

---
+++ kadai_sort/sort.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        
+        function sort_2way($array,$order)
+        {
+          if($order===TRUE){
+            sort($array);
+            echo"昇順ソート:<br>";
+          } else{
+            rsort($array);
+            echo"降順ソート:<br>";
+          }
+          foreach ($array as $value) {
+        echo $value . "<br>";
+          }
+        echo "<br>";
+          }
+
+        $nums = [15, 4, 18, 23, 10];
+
+        sort_2way($nums, TRUE); 
+        sort_2way($nums, FALSE);  
+        ?>
+    </p>
+</body>
+
+</html>
